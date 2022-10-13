ebd6a2a9ea7bd6766a2c34cbc8604b83ee6e060b
Kim Beomhee <nakaldi@gmail.com>
2022-10-13 23:12:39 +0900

    first commit

---
+++ src/main/java/com/thunderdragon/projectshop/ProjectShopApplication.java
@@ -0,0 +1,13 @@
+package com.thunderdragon.projectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectShopApplication.class, args);
+    }
+
+}
