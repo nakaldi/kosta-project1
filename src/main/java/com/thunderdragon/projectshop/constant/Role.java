47c9c1d83b0fa1be2f291cfbe84f1b94e93e8c6e
ysysysysy <97_ysoy@naver.com>
2022-10-20 18:12:50 +0900

    회원가입/로그인

---
+++ src/main/java/com/thunderdragon/projectshop/constant/Role.java
@@ -0,0 +1,5 @@
+package com.thunderdragon.projectshop.constant;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/thunderdragon/projectshop/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.thunderdragon.projectshop.repository;
+
+import com.thunderdragon.projectshop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+}
